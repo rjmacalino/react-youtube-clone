6a5fc8632605cfef0053ce644de6c04f465143c0
rjmacalino <rjhaymacalino@gmail.com>
2022-09-17 17:09:58 +1000

    improved whitespacing.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,50 +1,37 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useEffect, useState } from "react"
+import { Box, Stack, Typography } from "@mui/material"
 
-import { Box, Stack, Typography} from '@mui/material'
-import { SideBar, Videos } from './'
-
-import { fetchFromAPI } from '../utils/fetchFromAPI'
-
+import { fetchFromAPI } from "../utils/fetchFromAPI"
+import { Videos, SideBar } from "./"
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New")
+  const [videos, setVideos] = useState(null)
 
-  const [selectedCategory, setSelectedCategory] = useState('New')
-  const [videos, setVideos] = useState([])
+  useEffect(() => {
+    setVideos(null)
 
-  useEffect(()=> {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
       .then((data) => setVideos(data.items))
-  }, [selectedCategory])
+    }, [selectedCategory])
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}>
-        <SideBar 
-          selectedCategory=
-          {selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-        />
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
-        <Typography className='copyright' variant='body2'
-        sx={{ mt: 1.5, color: '#fff'}}>
-          Copyright 2020 RJM Media
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 JSM Media
         </Typography>
       </Box>
 
-      <Box p={2} sx={{ overflowY: 'auto',
-      height: '90vh', flex: 2}}>
-        <Typography variant="h4"
-        fontWeight="bold" mb={2} sx={{color: 'white'}}>
-          {selectedCategory} <span style={{ color: '#F31503'}}>
-            videos
-          </span>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
 
         <Videos videos={videos} />
-
       </Box>
-
     </Stack>
   )
 }
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,16 +1,17 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
 const options = {
   params: {
-    maxResults: '50'
+    maxResults: '30',
+    regionCode: 'PH'
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
-};
+}
 
 
 export const fetchFromAPI = async (url) => {
